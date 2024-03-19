d55e85a464f495b34b0d3407034e57a68334428d
Lori Li <lihaoan1001@163.com>
2024-03-18 17:18:45 -0700

    deploy on docker

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            This is a autograding platform for the student (I$apos;m a TA BTW)
+            This is a autograding platform for the student (I&apos;m a TA BTW)
           </p>
         </a>
 
