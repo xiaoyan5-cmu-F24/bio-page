4e83067067aaf2c448202ee3a6512b129573f75b
Scott(Xiaoyang) Chen <xiaoyan5@andrew.cmu.edu>
2024-09-20 13:21:39 -0700

    save

---
+++ example.test.js
@@ -0,0 +1,10 @@
+describe('Demo Test Suite', () => {
+
+    test('Demo test', () => {
+        expect(1).toBe(1);
+    });
+
+    test('Demo bad test', () => {
+        expect(8 + 8).toBe(0);
+    });
+});
